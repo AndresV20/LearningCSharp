f6c984fc3b5a9a6d212f390d8e6035b4f15545bd
Andres Villalobos <andres.villalobos@datagrove.com>
2022-12-20 11:56:42 -0600

    completed first module

--- main.cs
+++ main.cs
@@ -16,6 +16,57 @@ using System;
     {
         Console.WriteLine("Name:Andres");
         Console.WriteLine("Surname:Villalobos");
+        Console.WriteLine("age:" 500)
     }
 }
-//
+//2 One of the tasks is to fix code
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("My first C# Code Coach!");
+    }
+}
+// the error here is that WriteLine was missing, therefore the code wasn't printing anything
+
+//3 Namespaces lesson
+using System;
+// using can be the equivalent to import 
+//teaches about comments 
+/* and the two ways to add them 
+to your code*/
+
+//4 Variables lesson
+string name;
+int age;
+boolean isMan = true;
+list money;
+char label = 'A';
+double income = 100.75;
+name = "Andres";
+float height = 1.82f;
+decimal weight = 92.8m;
+
+//other way to declare variable
+string lastName = "Villalobos";
+Console.WriteLine(lastName);
+//5 doing math
+Console.WriteLine(5+4);
+Console.WriteLine(6-3);
+// When you run this it does the addition
+int x = 5;
+int y = 4;
+
+Console.WriteLine(x+y);
+// output = 9
+int beatsPerMinute = 80*60; // multiplication
+double divide = 20/50;
+//combined operations
+int a = 2;
+int b = 3;
+int c = 4;
+
+Console.WriteLine(a+b-c);
+// output = 1
