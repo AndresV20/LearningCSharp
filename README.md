953af727e9b230cdcbd301e73d0a286612fdc2f6
Andres Villalobos <andres.villalobos@datagrove.com>
2022-12-20 08:56:48 -0600

    first two lessons solo learn

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LeaningCSharp
+
+In this repo I'll store the snippets of code that solo learn teaches me
  
---
+++ main.cs
@@ -0,0 +1,21 @@
+// 1 Teaches you about printing 
+Console.WriteLine("Welcome to C#!");
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("This is a print statement");
+    }
+}
+using System;
+
+{
+    public static void multStatements(string[] args)
+    {
+        Console.WriteLine("Name:Andres");
+        Console.WriteLine("Surname:Villalobos");
+    }
+}
+//
