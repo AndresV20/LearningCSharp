189a2b32876312032860a8c07caebf7f8072a8bb
Andres Villalobos <andres.villalobos@datagrove.com>
2022-12-26 12:33:56 -0600

    code wars + notes
    Did 4 code wars exercises and also learned the difference between int and uint

--- Excercise1.cs
+++ Excercise1.cs
@@ -70,3 +70,20 @@ public class Kata
       return result == "Zach" ? 18 : 0;
   }
 }
+//if the name is zach then it will output 18 as the number of lightsabers but ant other string will out put 0 lightsabers
+
+//Exercise 4
+/* You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+Function should return true if it is possible and false if not. */ 
+public static class fuel
+{
+  public static bool ZeroFuel(uint distanceToPump, uint mpg, uint fuelLeft)
+  {
+    uint enFuel= mpg * fuelLeft;
+    bool result = distanceToPump <= enFuel? true: false;
+    return result;
+  }
+}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # LeaningCSharp
 
 In this repo I'll store the snippets of code that solo learn teaches me
+
+When a code wars challenge uses uint, use it in the solution because if you use int, it'll cause a series of errors
  
